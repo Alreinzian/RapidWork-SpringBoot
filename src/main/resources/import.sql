a2dce6e0a7d331c6127929b4880659128077d11d
Sagender <44262173@certus.edu.pe>
2022-11-17 18:59:09 -0500

    Creaciòn del Repositorio-RapidWork

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO Clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Daniel', 'Manrique', 'sagender@gmail.com', '2015-08-15');
+INSERT INTO Clientes (id, nombre, apellido, email, create_at) VALUES(2, 'Joe', 'Bunde', 'joe@gmail.com', '2016-10-18');
+INSERT INTO Clientes (id, nombre, apellido, email, create_at) VALUES(3, 'Renat', 'Spar', 'Roe@gmail.com', '2014-10-18');
+INSERT INTO Clientes (id, nombre, apellido, email, create_at) VALUES(4, 'Jefry', 'Dammer', 'Rloe@gmail.com', '2017-10-18');
+INSERT INTO Clientes (id, nombre, apellido, email, create_at) VALUES(5, 'Ren', 'Fammer', 'Ruloe@gmail.com', '2017-15-18');
